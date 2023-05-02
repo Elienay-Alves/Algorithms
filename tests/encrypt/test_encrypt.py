9fe84bcad6d2ac57cbc09f2828be53914e16fb14
Elienay Alves <elienay8486@gmail.com>
2023-05-02 00:21:31 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+
+
+KEY_ERROR_MSG = "tipo inválido para key"
+MSG_ERROR_MSG = "tipo inválido para message"
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message(message="abcd", key=3) == "cba_d"
+    assert encrypt_message(message="abcd", key=2) == "dc_ba"
+    assert encrypt_message(message="abcd", key=5) == "dcba"
+    assert encrypt_message("dcba", 5) != "dcba"
+
+    with pytest.raises(TypeError, match=KEY_ERROR_MSG):
+        encrypt_message("abcc", "abcd")
+
+    with pytest.raises(TypeError, match=MSG_ERROR_MSG):
+        encrypt_message(2, 2)
